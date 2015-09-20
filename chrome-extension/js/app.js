86f7b3d2736d23e7c2bf59eb95d1b4d9ca03ad97
1heart <yixin1996@gmail.com>
2015-09-19 22:14:29 -0400

    wip

---
+++ chrome-extension/js/app.js
@@ -0,0 +1,12 @@
+causes = {
+	"child_with_book": {
+		"name": "Name of cause",
+		"assertion": "Do good things.",
+		"description": "Pellentesque consectetur lectus eget consectetur commodo. In varius sollicitudin magna sit amet euismod.",
+		"img": "http://harambeeusa.org/wp/wp-content/uploads/2010/05/Child_with_book.jpg"
+	}
+}
+
+$(document).ready(
+
+);
