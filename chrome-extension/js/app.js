9baee37dcb5c12d58233fc651e7109ede9ecb508
1heart <yixin1996@gmail.com>
2015-09-20 03:01:35 -0400

    moved causes to its own file

--- chrome-extension/js/app.js
+++ chrome-extension/js/app.js
@@ -1,44 +1,5 @@
 URL = 'http://beautifulaltruism.dev:5000/causes'
 
-causes = {
-			"child_with_book": {
-				"name": "Education for All",
-				"assertion": "Support Education for those in Need",
-				"description": "Sponsor a poor child in a developing nation on the path to a brighter future.",
-				"img": "http://harambeeusa.org/wp/wp-content/uploads/2010/05/Child_with_book.jpg",
-		
-			},
-			"leadership_training_woman": {
-				"name": "Support Women's Leadership Training",
-				"assertion": "Let women learn to lead.",
-				"description": "The answer to growth and development lies in the equality of women engaging in society-reforming tasks and economical decision-making",
-				"img": "http://www.hope-project.org/wp-content/uploads/2014/05/DSC_4459_Girls_Studying.jpg",
-			},
-			"actions_against_hunger": {
-				"name": "Actions Against Hunger",
-				"assertion": "Feed the starving. ",
-				"description": "Help provide those starving access to clean water and nutritional security.",
-				"img": "http://www.care.org/sites/default/files/image/TCD-2012-BB--3386.jpg",
-			},
-			"nepal_earthquake": {
-				"name": "Support The Nepal Earthquake Relief Fund",
-				"assertion": "Get Nepal back on its feet.",
-				"description": "Help support the funding to provide the displaced the resources to survive.",
-				"img": "http://www.vogue.com/wp-content/uploads/2015/05/21/nepal-kathmandu-earthquake-2015.jpg",
-			},
-			"cupcake-girl": {
-				"name": "Buy A Cupcake And Support The Girl's Education",
-				"assertion": "Help provide girls education.",
-				"description": "Buy a cupcake and donate so that low-income countries can create their first-generation graduates and global leaders.",
-				"img": "http://www.smorgasblurb.com/wp-content/uploads/2011/06/dscn08520886.jpg",
-			},
-			"ebola-support": {
-				"name": "Support Community Justice in Liberia",
-				"assertion": "Donate to provide resources to stop conficts caused by the ebola outbreak.",
-				"description": "Help provide those affected by the tensions between communites resulting from the ebola outbreak so that the communites can coexxist peacefully.",
-				"img": "http://cdn.thedailybeast.com/content/dailybeast/articles/2014/10/28/are-mandatory-ebola-quarantines-legal/jcr:content/image.img.2000.jpg/1414498383227.cached.jpg",
-			}
-		}
 
 $(document).ready(
 	function() {
